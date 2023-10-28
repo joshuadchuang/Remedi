21d7e6179a9f9266303072f4600e95b2a24f2763
Joshua Chuang <jchuang77@berkeley.edu>
2023-10-27 17:41:55 -0700

    Initial Commit

---
+++ RemediApp/RemediAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemediAppApp.swift
+//  RemediApp
+//
+//  Created by Joshua Chuang on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RemediAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
