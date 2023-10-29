3492f3e7cff668ff17225a0a64c8672b5ce11e42
Daniel <trdanli2221@gmail.com>
2023-10-29 00:13:38 -0700

    added ui styling

--- RemediApp/User Interface/ContentView.swift
+++ RemediApp/User Interface/ContentView.swift
@@ -12,37 +12,76 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Welcome to Remedi")
-                    .font(.largeTitle)
-                    .padding()
-                
-                Text("Your personal medication reminder assistant.")
-                    .padding()
-                
-                Spacer()
-                
-                NavigationLink(destination: UserHomeView().environmentObject(viewModel)) {
-                    Text("I am the User")
+            ZStack {
+                BackgroundView()
+                VStack(spacing: 10) {
+                    TitleTextView(title: "Welcome to Remedi")
+                    
+                    Text("Your personal medication reminder assistant.")
                         .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .padding(.horizontal)
+                        .padding()
+                    
+                    VStack {
+                        Image(systemName: "pills.circle")
+                            .symbolRenderingMode(.palette)
+                            .resizable()
+                            .foregroundStyle(LinearGradient(gradient:
+                                Gradient(colors: [Color("OceanBlue"), Color("GrassGreen")]),
+                                startPoint: .topLeading,
+                                endPoint:.bottomTrailing))
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 220, height: 220)
+                            .padding(.top)
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: UserHomeView().environmentObject(viewModel)) {
+                        Text("I am the User")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, maxHeight: 50)
+                            .foregroundStyle(LinearGradient(gradient:
+                                Gradient(colors: [Color(""), Color("")]),
+                                startPoint: .topLeading,
+                                endPoint:.bottomTrailing))
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    
+                    NavigationLink(destination: CaretakerHomeView()) {
+                        Text("I am the Caretaker")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, maxHeight: 50)
+                            .background(Color.green)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    Spacer()
                 }
-                
-                NavigationLink(destination: CaretakerHomeView()) {
-                    Text("I am the Caretaker")
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                        .background(Color.green)
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-                }
-                
-                Spacer()
             }
         }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color("Black2"), Color("DarkAqua")]),
+                       startPoint: .topLeading,
+                       endPoint:.bottomTrailing)
+            .ignoresSafeArea()
+    }
+}
+
+struct TitleTextView: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
     }
 }
 
